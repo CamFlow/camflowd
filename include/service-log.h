c5bef3a39e969adaa5e2064ed2f9a53cc17fae9c
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2017-08-03 22:44:36 +0100

    fix issue with very large entris

--- include/service-log.h
+++ include/service-log.h
@@ -16,14 +16,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <syslog.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include "service-config.h"
 
-static FILE *__log_fp=NULL;
+static int __log_fd=0;
 
 static inline void _init_logs( void ){
-  __log_fp = fopen(__service_config.log, "a+");
-  if(!__log_fp){
+  __log_fd = open(__service_config.log, O_APPEND|O_CREAT|O_WRONLY, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+  if(__log_fd < 0){
     syslog(LOG_ERR, "Cannot open file\n");
     exit(-1);
   }
@@ -32,8 +35,16 @@ static inline void _init_logs( void ){
 }
 
 static inline void log_print_json(char* json){
-  fprintf(__log_fp, json);
-  fprintf(__log_fp, "\n");
-  fflush(__log_fp);
+  int len = strlen(json);
+  int rc;
+
+  while (len > 0) {
+    rc = write(__log_fd, json, len);
+    if(rc<0)
+      exit(-1);
+    len-=rc;
+  }
+  write(__log_fd, "\n", 1);
+  fsync(__log_fd);
 }
 #endif
