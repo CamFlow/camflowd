e253ecbcbf6d723597f750a8e4c97e51a25a6edd
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2022-04-12 09:11:07 -0700

    update license statement

--- include/service-mqtt.h
+++ include/service-mqtt.h
@@ -1,17 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0 */
 /*
-*
-* Author: Thomas Pasquier <tfjmp@cs.ubc.ca>
-*
-* Copyright (C) 2016-2017 Harvard University
-* Copyright (C) 2017-2018 University of Cambridge
-* Copyright (C) 2018-2021 University of Bristol
-* Copyright (C) 2021 University of British Columbia
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License version 2, as
-* published by the Free Software Foundation.
-*
-*/
+ * Copyright (C) 2015-2016 University of Cambridge,
+ * Copyright (C) 2016-2017 Harvard University,
+ * Copyright (C) 2017-2018 University of Cambridge,
+ * Copyright (C) 2018-2021 University of Bristol,
+ * Copyright (C) 2021-2022 University of British Columbia
+ *
+ * Author: Thomas Pasquier <tfjmp@cs.ubc.ca>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2, as
+ * published by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ */
 #ifndef __SERVICE_MQTT_H
 #define __SERVICE_MQTT_H
 #include <stdio.h>
